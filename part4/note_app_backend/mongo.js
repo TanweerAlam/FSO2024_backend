520249470f8604fa964e2b8b292ae0f6a1caa448
Md Tanweer Alam <tanweeralam1312@gmail.com>
2024-09-23 13:36:55 +0530

    part4- noteApp (supertest implemented)

--- part4/note_app_backend/mongo.js
+++ part4/note_app_backend/mongo.js
@@ -1,14 +1,17 @@
 const mongoose = require('mongoose')
 
-if (process.argv.length<3) {
-  console.log('give password as argument')
-  process.exit(1)
-}
+// if (process.argv.length<3) {
+//   console.log('give password as argument')
+//   process.exit(1)
+// }
 
-const password = process.argv[2]
+// const password = process.argv[2]
 
-const url =
-  `mongodb+srv://fullstack:${password}@cluster0.o1opl.mongodb.net/?retryWrites=true&w=majority`
+// const url =
+//   `mongodb+srv://fullstack:${password}@cluster0.o1opl.mongodb.net/?retryWrites=true&w=majority`
+
+const url = `mongodb+srv://fso2024_admin:fso2024_password@university-of-helsinki.0ejfv.mongodb.net/testNoteApp?
+retryWrites=true&w=majority&appName=UNIVERSITY-OF-HELSINKI`
 
 mongoose.set('strictQuery',false)
 mongoose.connect(url)
@@ -23,21 +26,21 @@ const Note = mongoose.model('Note', noteSchema)
 
 // eslint-disable-next-line no-unused-vars
 const note = new Note({
-  content: 'Mongoose makes things easy',
-  date: new Date(),
-  important: true,
+
+  content: 'Browser can execute only JavaScript',
+  important: false
 })
 
-/*
+
 note.save().then(result => {
   console.log('note saved!')
   mongoose.connection.close()
 })
-*/
 
-Note.find({}).then(result => {
-  result.forEach(note => {
-    console.log(note)
-  })
-  mongoose.connection.close()
-})
+
+// Note.find({}).then(result => {
+//   result.forEach(note => {
+//     console.log(note)
+//   })
+//   mongoose.connection.close()
+// })
